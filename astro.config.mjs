bc02d89ef25c855502624420695a8b4a8baae7b4
Pablo400-glitch <paisficretpablo@gmail.com>
2024-12-28 14:05:23 +0000

    Build adjustments

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
+  output: "static",
   site:"https://Pablo400-glitch.github.io",
   base:"",
   integrations: [tailwind()],
