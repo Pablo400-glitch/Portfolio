ec0720cf643b4a1a4a9f026d5ab6ded229772279
Pablo400-glitch <paisficretpablo@gmail.com>
2024-12-29 10:40:30 +0000

    New Build

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   output: "static",
-  site:"https://Pablo400-glitch.github.io",
+  site:"https://Pablo400-glitch.github.io/Portfolio",
   base: '/Portfolio',
   integrations: [tailwind()],
   redirects: {
