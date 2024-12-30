b3537315f11fe090ab33791ba991d3e95ebfc16c
Pablo400-glitch <paisficretpablo@gmail.com>
2024-12-30 12:42:16 +0000

    new Build

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ export default defineConfig({
   integrations: [tailwind()],
   redirects: {
     '/': process.env.NODE_ENV === 'production' ? '/Portfolio/es' : '/es',
-    '/Portfolio/es': process.env.NODE_ENV === 'production' ? '/Portfolio/es' : '/es',
-    '/Portfolio/en': process.env.NODE_ENV === 'production' ? '/Portfolio/en' : '/en',
+    '/es': process.env.NODE_ENV === 'production' ? '/Portfolio/es' : '/es',
+    '/en': process.env.NODE_ENV === 'production' ? '/Portfolio/en' : '/en',
   }
 });
